fcdd7c62b340f1d4729738e39605cec77bf42c3c
clevyoung <clevyoung@gmail.com>
2021-11-30 21:57:38 +0900

    타입을 만족하는 일부만을 선택적으로 포함할 수 있는 Partial 유틸리티 타입

--- 7_utility-type.ts
+++ 7_utility-type.ts
@@ -6,12 +6,13 @@ interface Product {
   stock: number;
 }
 
-// 상품 목록을 받아오기 위한 API 함수
+// 1. 상품 목록을 받아오기 위한 API 함수
 function fetchProduct(): Promise<Product[]> {}
 
 // Pick - 특정 타입에서 몇 개의 속성을 선택하여 타입을 정의할 수 있다.
 type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>;
 
+// 2. 특정 상품의 상세 정보를 나타내기 위한 함수
 function displayProductDetail(shoppingItem: ShoppingItem) {}
 
 // Omit - 특정 타입에서 지정된 속성만 제거한 타입을 정의할 수 있다.
@@ -33,3 +34,18 @@ const chingtao: Omit<AddressBook, 'address' | 'company'> = {
   name: '중국집',
   phone: 1224343,
 };
+
+// partial - 타입을 만족하는 일부만을 선택적으로 포함할 수 있다.
+type UpdateProduct = Partial<Product>;
+
+// Partial을 이용한다면 아래와 같이 정의할 필요가 없다.
+// interface UpdateProduct {
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number;
+// }
+
+// 3. 특정 상품 정보를 업데이트 하는 함수
+function updateProductItem(productItem: Partial<Product>) {}

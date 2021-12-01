8f437b8ff8748b043bdfdca006cc3cc02c90262b
clevyoung <clevyoung@gmail.com>
2021-12-01 14:18:01 +0900

    유틸리티 타입 구현하기

--- 7_utility-type.ts
+++ 7_utility-type.ts
@@ -49,3 +49,38 @@ type UpdateProduct = Partial<Product>;
 
 // 3. 특정 상품 정보를 업데이트 하는 함수
 function updateProductItem(productItem: Partial<Product>) {}
+
+// 4. 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+  username: string;
+  email: string;
+  profilePhotoUrl: string;
+}
+
+// interface UserProfileUpdate {
+//   username?: string;
+//   email?: string;
+//   profilePhotoUrl?: string;
+// }
+
+// #1
+// type UserProfileUpdate = {
+//   username?: UserProfile['username'];
+//   email?: UserProfile['email'];
+//   profilePhotoUrl?: UserProfile['profilePhotoUrl'];
+// };
+
+// #2
+// type UserProfileUpdate = {
+//   [p in 'username' | 'email' | 'profilePhotoUrl']?: UserProfile[p];
+// };
+
+// #3
+type UserProfileUpdate = {
+  [p in keyof UserProfile]?: UserProfile[p];
+};
+
+// #4 - Partial의 구현 모습
+type Subset<T> = {
+  [p in keyof T]?: T[p];
+};

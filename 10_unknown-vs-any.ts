94d19c7ed114a844cc9d43232fb38f94796304d8
clevyoung <clevyoung@gmail.com>
2022-04-22 23:40:15 +0900

    unknown 타입에 대한 설명 추가

--- 10_unknown-vs-any.ts
+++ 10_unknown-vs-any.ts
@@ -5,10 +5,22 @@ foo = 1; // number
 foo = 'string'; // string
 foo = {}; // object
 
-// unknown 타입으로 선언된 변수는 any를 제외한 다른 타입으로 선언된 변수에 할당될 수 없다.
+// 모든 타입은 unknown에 할당될 수 있다.
+let unknownValue: unknown;
+let neverValue: never;
+let anyValue: any;
+
+unknownValue = 1;
+unknownValue = '1';
+unknownValue = {};
+unknownValue = neverValue;
+unknownValue = anyValue;
+
+// unknown 타입으로 선언된 변수는 any, 그리고 unknown을 제외한 다른 타입으로 선언된 변수에 할당될 수 없다.
 let bar: unknown;
 
 let anyType: any = bar;
+let unknownType: unknown = bar;
 let booleanType: boolean = bar; //Type 'unknown' is not assignable to type 'boolean'
 let numberType: number = bar; // Type 'unknown' is not assignable to type 'number'
 let stringType: string = bar; // Type 'unknown' is not assignable to type 'string'
